3283f18f46ab7ae438ac5003f8465586b3007ae0
ForceGamer <forcegamer159@gmail.com>
2021-07-11 09:15:10 +0200

    Update post-setup.sh

--- post-setup.sh
+++ post-setup.sh
@@ -21,8 +21,18 @@ clear
 sudo pacman --noconfirm -S xorg-server lightdm lightdm-webkit2-greeter
 cd /usr/share/lightdm-webkit/themes/
 sudo git clone https://github.com/GabrielTenma/lightdm-gab-gradient
+echo "Manual intervention required. Instructions:"
 echo "Change webkit_theme to: lightdm-gab-gradient"
 read -p "Enter editor to use: " editor
 sudo pacman -S $editor
 sudo $editor /etc/lightdm/lightdm-webkit2-greeter.conf
+echo "Manual intervention required. Instructions:"
+echo "Under [Seat:*], uncomment #greeter-session=, and change its value to lightdm-webkit2-greeter"
+read -p "Press enter to continue"
+sudo $editor /etc/lightdm/lightdm.conf
 clear
+
+#-----
+#Fonts (required for lightdm theme, and some Office docs)
+#-----
+yay -S --noconfirm ttf-iosevka ttf-windows
