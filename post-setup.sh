a5a73b5cafc9815ee535388a96ae237ff15b436f
ForceGamer <forcegamer159@gmail.com>
2021-07-11 10:00:50 +0200

    Update post-setup.sh

--- post-setup.sh
+++ post-setup.sh
@@ -51,6 +51,16 @@ qt5-styleplugins lxsession xautolock rxvt-unicode-truecolor-wide-glyphs xclip sc
 thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler viewnior mpv mpd mpc ncmpcpp \
 pavucontrol parcellite neofetch w3m htop picom-git obmenu-generator gtk2-perl playerctl xsettingsd zsh
 
+#-------------
+#Theme install
+#-------------
+cd ~
+git clone https://github.com/owl4ce/dotfiles.git
+rsync -avxHAXP --exclude '.git*' --exclude 'LICENSE' --exclude '*.md' dotfiles/ ~/
+
+#---------
+#ZSH setup
+#---------
 chsh -s $(command -v zsh)
 chsh -s $(command -v zsh) &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -58,14 +68,14 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-cd ~
-git clone https://github.com/owl4ce/dotfiles.git
-rsync -avxHAXP --exclude '.git*' --exclude 'LICENSE' --exclude '*.md' dotfiles/ ~/
-
+#-------------
+#Icons install
+#-------------
 pushd ~/.icons/
 tar -Jxvf Papirus-Custom.tar.xz && tar -Jxvf Papirus-Dark-Custom.tar.xz
 sudo ln -vs ~/.icons/Papirus-Custom /usr/share/icons/Papirus-Custom
 sudo ln -vs ~/.icons/Papirus-Dark-Custom /usr/share/icons/Papirus-Dark-Custom
 popd
-fc-cache -rv
 sudo chmod u+s $(command -v brightnessctl)
+
+fc-cache -rv
