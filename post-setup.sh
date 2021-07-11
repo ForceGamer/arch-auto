c64eda008f9173cbc0caf0f6ff307e5ed8a1cc3e
ForceGamer <forcegamer159@gmail.com>
2021-07-11 09:01:36 +0200

    Update post-setup.sh

--- post-setup.sh
+++ post-setup.sh
@@ -1,22 +1,28 @@
 # Automatic setup of my preferred WM, DM, themes, and some extras
+USER=$(whoami)
 
 #---------
 #YAY setup
 #---------
 sudo pacman --noconfirm -Sy git
 sudo mkdir /temp
-sudo chown
+sudo chown $USER:$USER /temp
 cd /temp
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 cd ~
 rm -rf /temp/*
-
+clear
 
 #-------------
 #LightDM setup
 #-------------
-sudo pacman --noconfirm -S xorg-server lightdm lightdm-webkit-greeter
+sudo pacman --noconfirm -S xorg-server lightdm lightdm-webkit2-greeter
 cd /usr/share/lightdm-webkit/themes/
-git clone https://github.com/GabrielTenma/lightdm-gab-gradient
+sudo git clone https://github.com/GabrielTenma/lightdm-gab-gradient
+echo "Change webkit_theme to: lightdm-gab-gradient"
+read -p "Enter editor to use: " editor
+sudo pacman -S $editor
+sudo $editor /etc/lightdm/lightdm-webkit2-greeter.conf
+clear
